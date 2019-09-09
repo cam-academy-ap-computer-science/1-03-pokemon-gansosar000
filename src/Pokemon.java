aa29402d0ae1f4604faf9f54fcbd0a54a62a1657
gansosar000 <gansosar000@CAM-LAB-15.bgsd.k12.wa.us>
2019-09-09 07:34:45 -0700

    Edit added

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,9 +1,11 @@
 
 public class Pokemon {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		
 	}
 
 }
